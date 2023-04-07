2c2bc289281b21a14acc92526d619f3525272b3a
Ha-Hieu-Thanh <hieuthanh4a2@gmail.com>
2023-04-08 00:10:13 +0700

    lam quen state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,12 +4,22 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Hieu Thanh",
+    address: "Hanoi",
+    age: 20,
+  };
+
   // JSX (viet code js trong html)
   render() {
     return (
       <div>
-        my first component
-        {Math.random()}
+        My name is {this.state.name}
+        <br />
+        My address is {this.state.address}
+        <br />
+        My age is {this.state.age}
+        <br />
         {/* {} to write js code */}
       </div>
     );
