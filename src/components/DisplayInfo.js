c43de17211131d02612caeda8a8c67067cc623c9
Ha-Hieu-Thanh <hieuthanh4a2@gmail.com>
2023-04-08 14:33:00 +0700

    conditional

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,24 +1,43 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+  state = { isShowListUsers: true };
+
+  handleShowHide = () => {
+    this.setState({ isShowListUsers: !this.state.isShowListUsers });
+  };
+
   render() {
     // Destructuring
     const { listUsers } = this.props;
-    console.log(listUsers);
+    // console.log(listUsers);
     return (
       // props => properties
       <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <p>Id: {user.id}</p>
-              <p>Name: {user.name}</p>
-              <p>Age: {user.age}</p>
-              <p>Address: {user.address}</p>
-              <hr />
-            </div>
-          );
-        })}
+        <div>
+          <button
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowListUsers ? "Hide" : "Show"}
+          </button>
+        </div>
+        {this.state.isShowListUsers && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} class={+user.age >= 18 ? "green" : "red"}>
+                  <p>Id: {user.id}</p>
+                  <p>Name: {user.name}</p>
+                  <p>Age: {user.age}</p>
+                  <p>Address: {user.address}</p>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,9 +9,9 @@ class MyComponent extends React.Component {
   // Moi khi cap nhat state thi ham render se duoc goi lai
   state = {
     listUsers: [
-      { id: 1, name: "Hieu Thanh 1", age: 31, address: "Thai Nguyen 1" },
-      { id: 2, name: "Hieu Thanh 2", age: 32, address: "Thai Nguyen 2" },
-      { id: 3, name: "Hieu Thanh 3", age: 33, address: "Thai Nguyen 3" },
+      { id: 1, name: "Hieu Thanh 1", age: 16, address: "Thai Nguyen 1" },
+      { id: 2, name: "Hieu Thanh 2", age: 26, address: "Thai Nguyen 2" },
+      { id: 3, name: "Hieu Thanh 3", age: 69, address: "Thai Nguyen 3" },
     ],
   };
 
